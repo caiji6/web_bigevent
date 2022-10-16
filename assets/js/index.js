b6f13b87dd3831f1900f4af8565801b686bceed3
crj <740929987@qq.com>
2022-10-16 16:58:56 +0800

    完成了主页功能的开发

---
+++ assets/js/index.js
@@ -0,0 +1,64 @@
+$(function(){
+    //调用getUserInfo获取用户基本信息
+    getUserInfo()
+
+    var layer = layui.layer
+    $('#btnLogout').on('click', function(){
+        //提示用户是否确认退出
+        layer.confirm('确定退出登录?', {icon: 3, title:'提示'}, function(index){
+            // 1.清空本地存储中的token
+            localStorage.removeItem('token')
+            // 2.重新跳转到登录页面
+            location.href = '/login.html'
+            // 关闭 confirm询问框
+            layer.close(index);
+          });
+    })
+})
+
+
+//获取用户的基本信息
+function getUserInfo(){
+    $.ajax({
+        method: 'GET',
+        url: '/my/userinfo',
+        // Headers: {
+        //     Authorization: localStorage.getItem('token') || ''
+        // },
+        success: function(res){
+            if(res.status !== 0){
+                return layui.layer.msg('获取用户信息失败')
+            }
+
+            renderAvater(res.data)
+        },
+
+        //不论成功还是失败，最终都会调用complete函数
+        // complete: function(res){
+        //     if(res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！'){
+        //         localStorage.removeItem('token')
+        //         location.href = '/login.html'
+        //     }
+        // }
+
+    })
+}
+
+//渲染用户的头像
+function renderAvater(user){
+    // 1.获取用户的名称
+    var name = user.nickname || user.username
+    // 2.设置欢迎的文本
+    $('.welcome').html('欢迎&nbsp;&nbsp;'+ name)
+    // 3.按需渲染用户的头像
+    if(user.user_pic !== null){
+        // 3.1渲染图片头像
+        $('.layui-nav-img').attr('src',user.user_pic).show()
+        $('.text-avatar').hide()
+    }else {
+        // 3.2渲染文本头像
+        $('.layui-nav-img').hide()
+        var first = name[0].toUpperCase()
+        $('.text-avatar').html(first).show()
+    }
+}
